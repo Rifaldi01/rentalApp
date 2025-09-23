9ce17a561b67dd7f1244c32e22ac0b5d68b70c00
Rifaldi01 <rifaldipratama2004@gmail.com>
2025-09-23 10:26:14 +0700

    fix

--- resources/views/admin/accessories/index.blade.php
+++ resources/views/admin/accessories/index.blade.php
@@ -25,7 +25,7 @@
                         <th>Stok All</th>
                         <th>Stok Available</th>
                         <th>Rental</th>
-                        <th class="text-center" width="9%">Action</th>
+{{--                        <th class="text-center" width="9%">Action</th>--}}
                     </tr>
                 </thead>
                 <tbody>
@@ -36,37 +36,37 @@
                         <td>{{ $data['stokAll'] }}</td>
                         <td>{{ $data['stok'] }}</td>
                         <td>{{ $data['rentedQty'] }}</td>
-                        <td>
-                            <a href="{{ route('admin.acces.destroy', $data['id']) }}" data-confirm-delete="true" class="btn btn-danger btn-sm bx bx-trash" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete"></a>
-                            <button data-bs-toggle="modal" data-bs-target="#exampleVerticallycenteredModal{{ $data['id'] }}" class="btn btn-warning btn-sm float-end bx bx-edit ms-2" data-bs-placement="top" title="Edit"></button>
+{{--                        <td>--}}
+{{--                            <a href="{{ route('admin.acces.destroy', $data['id']) }}" data-confirm-delete="true" class="btn btn-danger btn-sm bx bx-trash" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete"></a>--}}
+{{--                            <button data-bs-toggle="modal" data-bs-target="#exampleVerticallycenteredModal{{ $data['id'] }}" class="btn btn-warning btn-sm float-end bx bx-edit ms-2" data-bs-placement="top" title="Edit"></button>--}}
 
-                            <!-- Edit Modal -->
-                            <div class="modal fade" id="exampleVerticallycenteredModal{{ $data['id'] }}" tabindex="-1" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title">Edit Accessories</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <form action="{{ route('admin.acces.update', $data['id']) }}" method="POST">
-                                            @csrf
-                                            @method('PUT')
-                                            <div class="modal-body">
-                                                <label class="col-form-label">Name Accessories</label>
-                                                <input value="{{ $data['name'] }}" type="text" name="name" class="form-control" placeholder="Enter Accessories">
-                                                <label class="col-form-label mt-2">Stok</label>
-                                                <input type="number" value="{{ $data['stok'] }}" name="stok" class="form-control" placeholder="">
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                <button type="submit" class="btn btn-primary">Save<i class="bx bx-save"></i></button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- End of Edit Modal -->
-                        </td>
+{{--                            <!-- Edit Modal -->--}}
+{{--                            <div class="modal fade" id="exampleVerticallycenteredModal{{ $data['id'] }}" tabindex="-1" aria-hidden="true">--}}
+{{--                                <div class="modal-dialog modal-dialog-centered">--}}
+{{--                                    <div class="modal-content">--}}
+{{--                                        <div class="modal-header">--}}
+{{--                                            <h5 class="modal-title">Edit Accessories</h5>--}}
+{{--                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>--}}
+{{--                                        </div>--}}
+{{--                                        <form action="{{ route('admin.acces.update', $data['id']) }}" method="POST">--}}
+{{--                                            @csrf--}}
+{{--                                            @method('PUT')--}}
+{{--                                            <div class="modal-body">--}}
+{{--                                                <label class="col-form-label">Name Accessories</label>--}}
+{{--                                                <input value="{{ $data['name'] }}" type="text" name="name" class="form-control" placeholder="Enter Accessories">--}}
+{{--                                                <label class="col-form-label mt-2">Stok</label>--}}
+{{--                                                <input type="number" value="{{ $data['stok'] }}" name="stok" class="form-control" placeholder="">--}}
+{{--                                            </div>--}}
+{{--                                            <div class="modal-footer">--}}
+{{--                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>--}}
+{{--                                                <button type="submit" class="btn btn-primary">Save<i class="bx bx-save"></i></button>--}}
+{{--                                            </div>--}}
+{{--                                        </form>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <!-- End of Edit Modal -->--}}
+{{--                        </td>--}}
                     </tr>
                     @endforeach
                 </tbody>
