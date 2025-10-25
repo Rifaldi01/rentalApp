32918e43c03c90041e79a9c347ec6c1aab26bc42
Rifaldi01 <rifaldipratama2004@gmail.com>
2025-10-25 10:54:43 +0700

    tambahan

--- app/Http/Controllers/Admin/ProblemController.php
+++ app/Http/Controllers/Admin/ProblemController.php
@@ -59,6 +59,9 @@ class ProblemController extends Controller
         $destroy = Problem::find($id);
         $destroy->status = 1;
         $destroy->save();
+        $rental = Rental::findOrFail($destroy->rental_id);
+        $rental->status = 4;
+        $rental->save();
 
         Alert::success('Success', 'Problem has been finished');
         return back();
